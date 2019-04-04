879d96e37befc1bfe32de55e928b07e91964d71f
Martin de Jonge <madejo@hotmail.com>
2019-04-04 10:40:30 +0200

    'v1.9'

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-/* 
+ 
 // in this model you can adjust the tables, for example the names:
     // Table Name
     protected $table = 'posts';
@@ -15,4 +15,8 @@ class Post extends Model
     // Timestamps
     public $timestamps = true;
 
- */}
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+ }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,27 @@
                         <a href="/posts/create" class="btn btn-primary">Create a Blog</a>
                     </div>  
                     <br>
-                    <h3>Your Blog Posts</h3>
-
+                    <h3>Your Blogs</h3>
+                        @if(count($posts) > 0)
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>Title</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                                @foreach($posts as $post)
+                                <tr>
+                                    <th>{{$post->title}}</th>
+                                    <th><a href="/posts/{{$post->id}}/edit" class="btn btn-secondary">Edit</a></th>
+                                    <th></th>
+                                </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <div class="jumbotron">
+                                <p>Please write your first Blog!</p>
+                            </div>
+                        @endif
                 </div>
             </div>
         </div>
